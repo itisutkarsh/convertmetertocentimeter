50d4d892255edbfbd8edec063a2e6d77fbe23ac7
Utkarsh <92393738+itisutkarsh@users.noreply.github.com>
2021-10-12 23:26:45 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char check;
+float m,cm;
+
+
+
+
+ printf(" enter value to convert meter to centimeter");
+ scanf("%f",&m);
+ cm=m*100;
+  printf("the %fm is %fin cm",m,cm);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
